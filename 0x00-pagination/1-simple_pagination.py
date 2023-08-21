b8050ef5bb05960ed6f266ea064785c0bb9fcba9
rungene <rungenesamuel@gmail.com>
2023-08-21 17:14:06 +0000

    Task 1. Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""
+1-simple_pagination
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Get index rane for a given page number and page_size
+    Args:
+        page(int): page number
+        page_size(int): page size
+
+    Return:
+        tuple of size two containing a start index and an end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ calculates the start and end indexes for the requested page,
+        and returns the corresponding page of data from the dataset."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+
+        if not self.__dataset:
+            self.dataset()
+        return self.__dataset[start_index:end_index]
