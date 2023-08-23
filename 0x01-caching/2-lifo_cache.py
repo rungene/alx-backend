ba70bda965b71dfe86bff6b628ec3fd1cb669221
rungene <rungenesamuel@gmail.com>
2023-08-23 16:53:49 +0000

    Corrections

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -18,7 +18,7 @@ class LIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             last_key = list(self.cache_data.keys())[-1]
             del self.cache_data[last_key]
             print('DISCARD: {}'.format(last_key))
