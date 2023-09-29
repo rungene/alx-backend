e9e6018c0a25bef244e36ee2a11d8eade10f6f87
rungene <rungenesamuel@gmail.com>
2023-09-29 18:16:45 +0000

    Corrections

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -53,22 +53,22 @@ jobs.forEach((jobData) => {
   const job = queue.create('push_notification_code_2', jobData);
 
   job.on('complete', () => {
-    console.log(`Notification job: ${job.id} completed`);
+    console.log(`Notification job #${job.id} completed`);
   });
 
   job.on('failed', (error) => {
-    console.log(`Notification job ${job.id} failed: ${error}`);
+    console.log(`Notification job #${job.id} failed: ${error}`);
   });
 
   job.on('progress', (progress) => {
-    console.log(`Notification job: ${job.id} ${progress}% complete`);
+    console.log(`Notification job #${job.id} ${progress}% complete`);
   });
 
   job.save((error) => {
     if (error) {
       console.error(`Error creating job: ${error}`);
     } else {
-      console.log(`Notification job created: ${job.id}`);
+      console.log(`Notification job created #${job.id}`);
     }
   });
 });
