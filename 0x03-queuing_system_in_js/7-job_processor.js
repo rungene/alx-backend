8b9f1ac68d01d12129589bf48dbbbd665fe0b1af
rungene <rungenesamuel@gmail.com>
2023-09-29 15:25:06 +0000

    Corrections

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -5,20 +5,13 @@ const queue = kue.createQueue();
 const blacklisted = [4153518780, 4153518781];
 
 function sendNotification(phoneNumber, message, job, done) {
-  try {
-    if (blacklisted.includes(phoneNumber)) {
-      throw new Error(`Phone number ${phoneNumber} is blacklisted`);
-    }
-    const progress = job.progress();
-    if (progress === 0.5) {
-      console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-      setTimeout(()=> {
-        done();  
-      }, 1000);
-    }
-  } catch (error) {
-    console.error(error.message);
-    done(error);
+  job.progress(0, 100);
+  if (blacklisted.includes(phoneNumber)) {
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else {
+    job.progress(50, 100);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
   }
 }
 
